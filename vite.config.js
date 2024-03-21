2260f676a50414dc3d00d1e7231b2b6fe1a4e0f6
Tim Havlicek <tim.h4vlicek@gmail.com>
2024-03-21 14:39:52 +0100

    vite-plugin copy

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueJsx from "@vitejs/plugin-vue-jsx";
+import vueJsx from "./vue-jsx/index.js";
 
 // https://vitejs.dev/config/
 export default defineConfig({
